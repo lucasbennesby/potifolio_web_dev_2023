db2ef658bc15718bec33b73fa0297e8dcee2c177
lucasbennesby <lucasbenesby@gmail.com>
2023-10-23 19:07:04 -0300

    adicionado sobre

--- src/Pages/Sobre/Sobre.js
+++ src/Pages/Sobre/Sobre.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import styles from "./Sobre.module.css";
 function Sobre() {
-  return <div>Sobre</div>;
+  return (
+    <div className={styles.text}>
+      <p>Olá, me chamo Lucas Bennesby Lima e nasci em porto velho</p>
+      <p>
+        Desde criança sempre tive um lado criativo e curioso com eletrônicos
+        mais aflorado, costumava desmontar brinquedos para saber como
+        funcionavam. Na adolescência me sentia mais familiarizado com a área de
+        exatas.
+      </p>
+      <p>
+        Em 2018 ingressei na UFSC cursando Engenharia Ferroviária, onde descobri
+        minha paixão por programação.
+      </p>
+      <p>
+        Atualmente curso Sistemas de Informação e minha área de atuação
+        preferida é o desenvolvimento, no momento de páginas web, como esta.
+      </p>
+    </div>
+  );
 }
 
 export default Sobre;
